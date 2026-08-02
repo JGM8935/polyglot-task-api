204b5959dceb25c1479a64169943fe6ea157e483
Jelle Goemaere <jelle.goemaere6@gmail.com>
2026-08-02 17:07:37 +0200

    Set up Spring Boot project: Maven config, application entrypoint, Swagger config

---
+++ spring-boot/src/main/java/com/taskapi/config/OpenApiConfig.java
@@ -0,0 +1,32 @@
+package com.taskapi.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String BEARER_SCHEME = "bearerAuth";
+
+    @Bean
+    public OpenAPI taskApiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task & Project Management API")
+                        .description("Spring Boot implementation of the Polyglot Task API - a task/project " +
+                                "management system with JWT authentication and role-based authorization.")
+                        .version("v1.0.0"))
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_SCHEME))
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_SCHEME, new SecurityScheme()
+                                .name(BEARER_SCHEME)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
+    }
+}
